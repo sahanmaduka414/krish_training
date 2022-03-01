0fcbd4f45289a3c380a760c13c3fd239bddc9dc8
sahanmaduka414 <82067926+sahanmaduka414@users.noreply.github.com>
2022-03-01 11:29:27 +0530

    Assignment1

---
+++ Assignment/B/B.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package B;
+
+import A.A;
+
+
+
+public class B extends A{
+   @Override
+   public void run(){
+        System.out.println("B is running");
+    }
+}
